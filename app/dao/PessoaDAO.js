50a1d32fc1ea08a408c477f7318d58bce2da2428
10110Lucas <lucas_900azul@hotmail.com>
2019-08-13 18:21:37 -0300

    Usando MySQL

--- app/dao/PessoaDAO.js
+++ app/dao/PessoaDAO.js
@@ -1,36 +1,44 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'pessoas'
-});
+class PessoaDAO{
 
-function consultar(){
-    connection.connect();
-    connection.query('SELECT * FROM pessoa', (error, results, fields) => {
-        if (error) throw error;
-        console.log(results);
-    });
-    connection.end();
-}
+    constructor(){
+        this._connection = mysql.createConnection({
+            host: 'localhost',
+            port: '3306',
+            user: 'root',
+            password: '',
+            database: 'pessoas',
+            debug: false
+        });
+    }
 
-function salvar(name, email, tell){
-    connection.connect();
-
-    let json = {nome: `${name}`,
-                email: `${email}`,
-                telefone: parseInt(tell)};
-
-    connection.query('INSERT INTO pessoa SET ?', json, (error, results, fields) => {
+    consultar(){
+        this._connection.connect();
+        this._connection.query('SELECT * FROM pessoa', (error, results, fields) => {
             if (error) throw error;
             console.log(results);
-    });
+        });
+        this._connection.end();
+    }
 
-    connection.end();
+    salvar(name, email, tell){
+        this._connection.connect();
+
+        this._connection.query('INSERT INTO pessoa (nome, email, telefone) value (?, ?, ?)', 
+        [name, email, tell],
+        (error, results, fields) => {
+            if(error){
+                console.log('Usuário não foi inserido!');
+                throw error;
+            } else {
+                console.log('Usuário inserido com sucesso!\n', results.insertId);
+            }
+        });
+
+        this._connection.end();
+    }
+
+    // salvar("teste0001", "james@bgr.com", "987654321");
+    // consultar();
 }
-
-// salvar("teste0001", "james@bgr.com", "987654321");
-consultar();
